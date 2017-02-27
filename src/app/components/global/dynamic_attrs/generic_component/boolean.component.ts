d64c26f336da7554fe076b855ca1ba9695247a3e
Nao_Lucho <louis.watrin@gmail.com>
2017-02-27 13:22:43 +0300

    Presentation org
    html version

---
+++ src/app/components/global/dynamic_attrs/generic_component/boolean.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'my-time-attr',
+  templateUrl: 'builder.list_attr.component.html'
+})
+
+export class BooleanComponent {
+  label = "label";
+  //props: any;
+}
