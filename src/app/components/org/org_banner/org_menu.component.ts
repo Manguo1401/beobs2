8d1f586c5e25d08f6f214dd87f6c280888062bbc
Nao_Lucho <louis.watrin@gmail.com>
2017-03-30 18:19:47 +0300

    fil actu

--- src/app/components/org/org_banner/org_menu.component.ts
+++ src/app/components/org/org_banner/org_menu.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'my-org-menu-component',
-  templateUrl: 'org_menu.component.html'
+  templateUrl: 'org_menu.component.html',
+  styleUrls: ['../../css/button.style.css'],
+
 })
 
 export class OrgMenuComponent {}
